bcbee51bbb9acbdef6a7c389d918a0bfcddf19e4
NxPKG <116948796+NxPKG@users.noreply.github.com>
2025-03-20 13:38:02 +0600

    Added support for AWS CodeBuild in the application scope
    Signed-off-by: NxPKG <116948796+NxPKG@users.noreply.github.com>

--- khulnasoft/data_application_scope_test.go
+++ khulnasoft/data_application_scope_test.go
@@ -3,12 +3,10 @@ package khulnasoft
 import (
 	"fmt"
 	"testing"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
-func TestDataApplicationScopePolicy(t *testing.T) {
+func TestDataApplicationScope(t *testing.T) {
 	t.Parallel()
 	name := "Global"
 	resource.Test(t, resource.TestCase{
@@ -16,37 +14,110 @@ func TestDataApplicationScopePolicy(t *testing.T) {
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckApplicationScopePolicyDataSource(name),
-				Check:  testAccCheckApplicationScopePolicyDataSourceExists("data.khulnasoft_application_scope.defaultiap"),
+				Config: testAccCheckApplicationScopeDataSource(name),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckApplicationScopeExists("data.khulnasoft_application_scope.defaultiap"),
+					// Basic attribute checks
+					resource.TestCheckResourceAttr("data.khulnasoft_application_scope.defaultiap", "name", name),
+					resource.TestCheckResourceAttrSet("data.khulnasoft_application_scope.defaultiap", "description"),
+					resource.TestCheckResourceAttrSet("data.khulnasoft_application_scope.defaultiap", "author"),
+					// Categories existence check
+					resource.TestCheckResourceAttrSet("data.khulnasoft_application_scope.defaultiap", "categories.#"),
+				),
 			},
 		},
 	})
 }
 
-func testAccCheckApplicationScopePolicyDataSource(name string) string {
+// Test specific for codebuild data source functionality
+func TestDataApplicationScopeWithCodeBuild(t *testing.T) {
+	t.Parallel()
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				// First create a scope with codebuild
+				Config: testAccCheckApplicationScopeWithCodeBuild(),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckApplicationScopeExists("khulnasoft_application_scope.test_codebuild"),
+				),
+			},
+			{
+				// Then test reading it as a data source
+				Config: testAccCheckApplicationScopeWithCodeBuildDataSource(),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckApplicationScopeExists("data.khulnasoft_application_scope.test_codebuild_ds"),
+					// Verify codebuild specific attributes
+					resource.TestCheckResourceAttr(
+						"data.khulnasoft_application_scope.test_codebuild_ds",
+						"categories.0.artifacts.0.codebuild.0.expression",
+						"v1",
+					),
+					resource.TestCheckResourceAttr(
+						"data.khulnasoft_application_scope.test_codebuild_ds",
+						"categories.0.artifacts.0.codebuild.0.variables.0.attribute",
+						"aqua.topic",
+					),
+					resource.TestCheckResourceAttr(
+						"data.khulnasoft_application_scope.test_codebuild_ds",
+						"categories.0.artifacts.0.codebuild.0.variables.0.value",
+						"topic1",
+					),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckApplicationScopeDataSource(name string) string {
 	return fmt.Sprintf(`
 	data "khulnasoft_application_scope" "defaultiap" {
 		name = "%s"
 	}
-	output "appscopes" {
-		value = data.khulnasoft_application_scope.defaultiap
-	}
 	`, name)
-
 }
 
-func testAccCheckApplicationScopePolicyDataSourceExists(n string) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		rs, ok := s.RootModule().Resources[n]
-
-		if !ok {
-			return NewNotFoundErrorf("%s in state", n)
+func testAccCheckApplicationScopeWithCodeBuild() string {
+	return `
+	resource "khulnasoft_application_scope" "test_codebuild" {
+		description = "test codebuild application scope"
+		name        = "test_codebuild"
+		
+		categories {
+			artifacts {
+				codebuild {
+					expression = "v1"
+					variables {
+						attribute = "aqua.topic"
+						value     = "topic1"
+					}
+				}
+			}
 		}
+	}`
+}
 
-		if rs.Primary.ID == "" {
-			return NewNotFoundErrorf("ID for %s in state", n)
+func testAccCheckApplicationScopeWithCodeBuildDataSource() string {
+	return `
+	resource "khulnasoft_application_scope" "test_codebuild" {
+		description = "test codebuild application scope"
+		name        = "test_codebuild"
+		
+		categories {
+			artifacts {
+				codebuild {
+					expression = "v1"
+					variables {
+						attribute = "aqua.topic"
+						value     = "topic1"
+					}
+				}
+			}
 		}
-
-		return nil
 	}
+
+	data "khulnasoft_application_scope" "test_codebuild_ds" {
+		name = khulnasoft_application_scope.test_codebuild.name
+	}`
 }
